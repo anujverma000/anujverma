784bded400bd235fa212025290a469fd8666dd88
Anuj Verma <anujverma000@gmail.com>
2019-08-14 23:44:03 +0400

    classes-linked-list implementation

--- src/routes/posts/2019-02-05-react-lifecycles/post.js
+++
@@ -1,6 +0,0 @@
-export default {
-  title: `React new Lifecycle methds`,
-  tags: ['React', 'Javascript', 'Lifecycle'],
-  spoiler: "Let's see the practical ways to use react lifecycle methods.",
-  getContent: () => import('./document.mdx'),
-}
--- src/routes/posts/2019-08-13-cache-api/post.js
+++
@@ -1,6 +0,0 @@
-export default {
-  title: `Javascript Cache API`,
-  tags: ['Javascript', 'Cache'],
-  spoiler: "A native way to cache and use persistent data in javascript.",
-  getContent: () => import('./document.mdx'),
-}
