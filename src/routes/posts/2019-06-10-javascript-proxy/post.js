8549e8a2a271d3e2c5bcf5801d5659203e702ffd
Anuj Verma <anujverma000@gmail.com>
2019-08-18 23:28:00 +0400

    js-proxy-1

---
+++ src/routes/posts/2019-06-10-javascript-proxy/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Power of proxy in javascript`,
+  tags: ['Javascript', 'Proxy', 'whenAvailable'],
+  spoiler: "ES6 has introduced an important feature as Proxy Object. Which allows to override the fundamental opertaions like getting or setting the value on object.",
+  getContent: () => import('./document.mdx'),
+}
