ad3617dc4e57cbc0c7c564f3626cde7e03f9a60f
Anuj Verma <anujverma000@gmail.com>
2019-09-09 22:19:58 +0400

    express-graphql

--- src/routes/posts/2019-09-08-express-graphql-by-example/post.js
+++ src/routes/posts/2019-09-08-express-graphql-by-example/post.js
@@ -1,5 +1,5 @@
 export default {
-  title: `Express-GraphQl by excample`,
+  title: `Express-GraphQl by example`,
   tags: ['Express', 'GraphQl', 'node'],
   spoiler: "Learn Graphql with express-graphql and implement catalog with search and pagination.",
   getContent: () => import('./document.mdx'),
