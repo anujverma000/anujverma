5b6df524ca4fd8ffe64b9087a6e271a36ea537e8
Anuj Verma <anujverma000@gmail.com>
2019-09-09 22:18:30 +0400

    express-graphql

--- src/routes/posts/2018-11-20-implement-linked-list-in-class/post.js
+++ src/routes/posts/2018-11-20-implement-linked-list-in-class/post.js
@@ -1,6 +1,6 @@
 export default {
   title: `LinkedList implementation with ES6 Classes`,
   tags: ['Data Structure', 'LinkedList', 'ES6'],
-  spoiler: "Let try to implement linkedlist datastructure using ES6 classes",
+  spoiler: "Let's try to implement linkedlist datastructure using ES6 classes",
   getContent: () => import('./document.mdx'),
 }
---
+++ src/routes/posts/2019-09-08-express-graphql-by-example/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Express-GraphQl by excample`,
+  tags: ['Express', 'GraphQl', 'node'],
+  spoiler: "Learn Graphql with express-graphql and implement catalog with search and pagination.",
+  getContent: () => import('./document.mdx'),
+}
