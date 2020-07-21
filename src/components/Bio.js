69b08c75089ae3f3eb8824d048e571c206a2a400
Anuj Verma <anujverma000@gmail.com>
2020-07-22 02:59:24 +0400

    updated profile page

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -14,13 +14,16 @@ function Bio(props) {
       ${props.className || ''}
     `}>
       <img loading="lazy" src={photoURL} alt="Me" />
-      <div className={`${styles.bioDetails}`}>
-        <h1 className={`${styles.titleName}`}>Anuj Verma</h1>
-        <div className={`${styles.bioDesc}`}>
-          <p>Software specialist with over 10 years experience in commercial software development.</p>
-          <p>Have a solid background in the analysis, design, modeling and development.</p>
-          <p>Strong Experience in developing applications of different business dynamics, including enterprise and end-user facing applications ranging from retail, e-commerce and banking. </p>
-          <p>Worked in enterprise and startups both environments where I have taken projects from inception to final product. </p>
+      <div className={styles.bioDetails}>
+        <h1 className={styles.titleName}>Anuj Verma</h1>
+        <div className={styles.bioDesc}>
+          <p>Detailed oriented and self motivated software professional with over 10 years experience in analysis, design and development.</p>
+          <p>Passionate for result-oriented approach with a focus on quality and user experience.</p>
+          <p>Product design, architect and leading development with focus on tracking, AB testing, SEO and AdOPs are some of the areas of expertise.</p>
+        </div>
+        <div>
+          <a className={styles.primaryButton} href="AnujVerma.pdf" download>Resume</a>
+          <a className={styles.button} href="/about">About Me</a>
         </div>
       </div>
     </div>
