017c7208596d689a15a6cb73997253dba24f7fa3
Anuj Verma <anujverma000@gmail.com>
2020-08-06 02:01:24 +0400

    react native performance

---
+++ src/routes/posts/2020-01-06-react-native-performance/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `React Native Performance Improvements`,
+  tags: ['React Native', 'Performance', 'React'],
+  spoiler: "How to handle the the performance issues with react native",
+  getContent: () => import('./document.mdx'),
+}
