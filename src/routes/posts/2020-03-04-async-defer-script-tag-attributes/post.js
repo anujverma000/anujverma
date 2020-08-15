49572b71b6f1866e54899be37dd14e9aa31ff343
Anuj Verma <anujverma000@gmail.com>
2020-08-15 23:40:44 +0400

    async defer

---
+++ src/routes/posts/2020-03-04-async-defer-script-tag-attributes/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `async defer or nothing`,
+  tags: ['Javascript', 'async', 'defer', 'script', 'html'],
+  spoiler: "Find out why async is better",
+  getContent: () => import('./document.mdx'),
+}
